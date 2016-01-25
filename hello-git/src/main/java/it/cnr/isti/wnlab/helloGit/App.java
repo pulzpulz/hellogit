2f972e9f1c07c9b7853c3e00ba96d855cdadb8b7
Giancarlo <giancarlo.riolo@isti.cnr.it>
2016-01-25 17:47:52 +0100

    mvn hellogit

---
+++ hello-git/src/main/java/it/cnr/isti/wnlab/helloGit/App.java
@@ -0,0 +1,13 @@
+package it.cnr.isti.wnlab.helloGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ helloGit.java
@@ -0,0 +1,12 @@
+package it.cnr.isti.wnlab.hellogit;
+
+
+
+public class hellogit {
+
+	public static void main(String[] args) {
+ 
+		
+	
+
+}
